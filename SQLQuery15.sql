a42204ef7bb9dc9962e95b486512e7433477f087
thanhbinh28b <thanhbinh28b@gmail.com>
2025-07-31 04:27:41 +0700

    Create SQL learning

---
+++ SQLQuery15.sql
@@ -0,0 +1,41 @@
+﻿-- SQL Query: Thứ tự thực thi trong câu truy vấn SQL và các vấn đề cần lưu ý
+SELECT [p].[ProductID], [p].[ProductName] --2
+FROM [dbo].[Products] AS [p]; --1
+
+SELECT [p].[ProductID], [p].[ProductName] --3
+FROM [dbo].[Products] AS [p] --1
+WHERE [p].[CategoryID]=1; --2
+
+
+SELECT [p].[CategoryID], COUNT([p].[ProductID]) AS [TotalProducts] --4
+FROM [dbo].[Products] AS [p] --1
+WHERE [p].[CategoryID] IN (1,2,3) --2
+GROUP BY [p].[CategoryID]; --3
+
+SELECT [p].[CategoryID], COUNT([p].[ProductID]) AS [TotalProducts] --5
+FROM [dbo].[Products] AS [p] --1
+WHERE [p].[CategoryID] IN (1,2,3) --2
+GROUP BY [p].[CategoryID] --3
+HAVING [TotalProducts]>=13; --4 [TotalProducts] chưa tồn tại
+
+SELECT [p].[CategoryID], COUNT([p].[ProductID]) AS [TotalProducts] --5
+FROM [dbo].[Products] AS [p] --1
+WHERE [p].[CategoryID] IN (1,2,3) --2
+GROUP BY [p].[CategoryID] --3
+HAVING COUNT([p].[ProductID])>=13; --4 
+
+SELECT [p].[CategoryID], COUNT([p].[ProductID]) AS [TotalProducts] --5
+FROM [dbo].[Products] AS [p] --1
+WHERE [p].[CategoryID] IN (1,2,3,4,5,6,7) --2
+GROUP BY [p].[CategoryID] --3
+HAVING COUNT([p].[ProductID])>=10 --4 [TotalProducts] chưa tồn tại
+ORDER BY [TotalProducts] ASC; --6  [TotalProducts] đã tồn tại
+
+SELECT TOP 3 p.ProductID, o.Discount, o2.ShipCountry --9 --10 
+FROM [dbo].[Products] AS [p] --1
+INNER JOIN [dbo].[Order Details] o --4 --2
+ON p.ProductID=o. ProductID   -- 3
+INNER JOIN [dbo].[Orders] o2 --7 --5
+ON o.OrderID=o2.OrderID --6
+WHERE o.Discount=0.25 and o2.ShipCountry='Germany' --8
+ORDER BY p.ProductID DESC; -- 11
---
+++ SQLQuery31.sql
@@ -0,0 +1,104 @@
+﻿-- VD1: Trigger khi insert sẽ chuyển productName về viết hoa toàn bộ
+CREATE TRIGGER MakeProductNameUpperCase
+ON [dbo].[Products]
+AFTER INSERT
+AS
+BEGIN
+	UPDATE Products
+	SET ProductName = UPPER(i.ProductName)
+	FROM inserted i
+	WHERE Products.ProductID = i.ProductID
+END
+
+INSERT [dbo].[Products]([ProductName], [SupplierID], [CategoryID])
+VALUES ('test name', 1, 1)
+
+-- VD2: Chặn không cho phép cập nhật Discontinued nhỏ hơn 0 (đề bài sai)
+CREATE TRIGGER PreventDiscountinued
+ON [dbo].[Products]
+FOR UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF EXISTS(SELECT 1 FROM inserted WHERE [Discontinued]!=0 AND [Discontinued]!=1)
+	BEGIN
+		ROLLBACK;
+		RAISERROR('[Discountined] không thể nhỏ hơn 0', -- Message text
+		16, -- Severity
+		1 -- State.
+		);
+	END
+END
+
+UPDATE [dbo].[Products]
+SET [Discontinued]=2
+WHERE [ProductID]=79
+
+-- Viết mộg trigger trong CSDL để đảm bảo rằng mỗi khi có 1 chi tiết đơn hàng mới được thêm vào,
+-- số lượng tồn kho phải được giảm đi 
+CREATE TRIGGER UpdateProductInventory
+ON [dbo].[Order Details]
+AFTER INSERT
+AS
+BEGIN
+	UPDATE [dbo].[Products]
+	SET [UnitsInStock] = [UnitsInStock]-(SELECT [Quantity] FROM inserted WHERE [dbo].[Products].ProductID = inserted.ProductID)
+	WHERE [dbo].[Products].[ProductID] IN (SELECT [ProductID] FROM inserted)
+END
+
+
+INSERT INTO [dbo].[Order Details]([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount])
+VALUES (10248, 78, 10, 50, 0);
+
+-- Bài tập
+-- BT1: Bổ sung thêm cột LastModified và tạo một trigger để sau khi một sản phẩm dược thêm vào hoặc cập nhật vào bảng "Products" tự động cập nhật trường "LastModified" với ngày và giờ hiện tại
+-- Bổ sung thêm cột LastModified
+ALTER TABLE [dbo].[Products]
+DROP COLUMN  [LastModified];
+
+ALTER TABLE [dbo].[Products]
+ADD [LastModified] datetime;
+
+CREATE TRIGGER AddLastModified
+ON [dbo].[Products]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+	UPDATE [Products]
+	SET [LastModified]=GETDATE()
+	FROM [Products]
+	JOIN inserted i
+	ON Products.ProductID = i.ProductID
+END
+
+UPDATE [dbo].[Products]
+SET [UnitsInStock]=200
+WHERE [ProductID]=79
+
+-- BT2: Tạo một trigger "INSTEAD OF DELETE" để kiểm tra xem khách hàng có đơn hàng (Orders) không. Nếu có, trigger không cho phép xoá khách hàng. Nếu không có đơn hàng liên quan, trigger thực hiện xoá khách hàng.
+CREATE TRIGGER DeleteCustomers
+ON [dbo].[Customers]
+INSTEAD OF DELETE
+AS
+BEGIN
+	IF EXISTS(
+		SELECT 1 
+		FROM [Orders] WHERE [Orders].[CustomerID] IN (SELECT CustomerID FROM deleted))
+	BEGIN
+	-- Nếu khách hàng có đơn hàng khôgn cho phép xoá
+		ROLLBACK;
+		RAISERROR('Không thể xoá khách hàng vì có đơn hàng liên quan', -- Message text
+		16, -- Severity
+		1 -- State.
+		);
+	END
+	ELSE
+	BEGIN
+		DELETE FROM Customers WHERE CustomerID IN (SELECT CustomerID FROM deleted);
+	END
+END
+
+DELETE FROM [dbo].[Customers]
+WHERE [CustomerID] LIKE 'ALFKI'
+
+-- BT3: Tạo một trigger trong cơ sở dữ liệu Northwind để kiểm tra và không cho phép đặt hàng (Order) với số lượng sản phẩm lớn hơn số lượng tồn kho
